264a3f40ba8870b06c930d5e565677268001faaa
kamea99 <kameanw99@gmail.com>
2024-03-02 20:06:42 -0700

    modified:   tracker.html 	modified:   tracker.js

--- tracker.js
+++ tracker.js
@@ -1,6 +1,46 @@
-var myModal = document.getElementById('myModal')
-var myInput = document.getElementById('myInput')
+// Function to handle form submission and add a new habit to the table
+function addHabit() {
 
-myModal.addEventListener('shown.bs.modal', function () {
-  myInput.focus()
-})
+    //habit input value
+    let habitInput = document.getElementById("habitIn").value;
+    let tableBody = document.getElementById("tableBody");
+    let row = document.createElement("tr");
+    
+    //let cell1 = document.createElement("td");
+
+    // Populate the <tr> with habit data
+    row.innerHTML = `
+        <td>${habitInput}</td>
+        <td>
+            <label for="sun"></label>
+            <input type="checkbox" id="sun">
+        </td>
+        <td>
+            <label for="mon"></label>
+            <input type="checkbox" id="mon">
+        </td>
+        <td>
+            <label for="tues"></label>
+            <input type="checkbox" id="tues">
+        </td>
+        <td>
+            <label for="wed"></label>
+            <input type="checkbox" id="wed">
+        </td>
+        <td>
+            <label for="thurs"></label>
+            <input type="checkbox" id="thurs">
+        </td>
+        <td>
+            <label for="fri"></label>
+            <input type="checkbox" id="fri">
+        </td>
+        <td>
+            <label for="sat"></label>
+            <input type="checkbox" id="sat">
+        </td>
+    `;
+
+    //row.appendChild(cell1);
+    tableBody.appendChild(row);
+}
